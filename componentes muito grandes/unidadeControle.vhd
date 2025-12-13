cab9998c722852a7360abcd5ed75e496a05eb26b
Rodk006 <rodrigo.haruna@gmail.com>
2025-12-13 19:11:32 -0300

    erros de sintaxe corrigidos

--- componentes muito grandes/unidadeControle.vhd
+++ componentes muito grandes/unidadeControle.vhd
@@ -2,18 +2,18 @@ library ieee;
 
 entity unidadeControle is
     port(
-        opcode       : in bit_vector (10 downto 0); -- sinal de condição código da instrução
-        extendMSB    : out bit_vector (4 downto 0); -- sinal de controle sign-extend
-        extendLSB    : out bit_vector (4 downto 0); -- sinal de controle sign-extend
-        reg2Loc      : out bit; -- sinal de controle MUX Read Register 2
-        regWrite     : out bit; -- sinal de controle Write Register
-        aluSrc       : out bit; -- sinal de controle MUX entrada B ULA
-        alu_control  : out bit_vector (3 downto 0); -- sinal de controle da ULA
-        branch       : out bit; -- sinal de controle desvio condicional
+        opcode : in bit_vector (10 downto 0); -- sinal de condição código da instrução
+        extendMSB : out bit_vector (4 downto 0); -- sinal de controle sign-extend
+        extendLSB : out bit_vector (4 downto 0); -- sinal de controle sign-extend
+        reg2Loc : out bit; -- sinal de controle MUX Read Register 2
+        regWrite : out bit; -- sinal de controle Write Register
+        aluSrc : out bit; -- sinal de controle MUX entrada B ULA
+        alu_control : out bit_vector (3 downto 0); -- sinal de controle da ULA
+        branch : out bit; -- sinal de controle desvio condicional
         uncondBranch : out bit; -- sinal de controle desvio incondicional
-        memRead      : out bit; -- sinal de controle leitura RAM dados
-        memWrite     : out bit; -- sinal de controle escrita RAM dados
-        memToReg     : out bit -- sinal de controle MUX Write Data
+        memRead : out bit; -- sinal de controle leitura RAM dados
+        memWrite : out bit; -- sinal de controle escrita RAM dados
+        memToReg : out bit -- sinal de controle MUX Write Data
     );
 end entity unidadeControle;
 
