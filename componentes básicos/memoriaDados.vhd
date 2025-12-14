ba45e3bf8155d4911a4606dc5109f3dca0c76fd5
Rodk006 <rodrigo.haruna@gmail.com>
2025-12-14 03:53:43 -0300

    adicionados sinais internos para melhor visualização no gtkwave

--- componentes básicos/memoriaDados.vhd
+++ componentes básicos/memoriaDados.vhd
@@ -1,5 +1,6 @@
 library ieee;
 use std.textio.all;	
+use ieee.numeric_bit.all;
 
 entity memoriaDados is
 	generic (
@@ -51,8 +52,37 @@ end function;
 
 -- inicializa a memoria
 signal memoriaD : mem_type := init_mem(datFileName);
+signal mem0, mem8, mem16, mem24, mem32, mem40, mem48, mem56, mem64 : bit_vector(63 downto 0);
 
 begin
+	mem0  <= memoriaD(0)  & memoriaD(1)  & memoriaD(2)  & memoriaD(3)  &
+			memoriaD(4)  & memoriaD(5)  & memoriaD(6)  & memoriaD(7);
+
+	mem8  <= memoriaD(8)  & memoriaD(9)  & memoriaD(10) & memoriaD(11) &
+			memoriaD(12) & memoriaD(13) & memoriaD(14) & memoriaD(15);
+
+	mem16 <= memoriaD(16) & memoriaD(17) & memoriaD(18) & memoriaD(19) &
+			memoriaD(20) & memoriaD(21) & memoriaD(22) & memoriaD(23);
+
+	mem24 <= memoriaD(24) & memoriaD(25) & memoriaD(26) & memoriaD(27) &
+			memoriaD(28) & memoriaD(29) & memoriaD(30) & memoriaD(31);
+
+	mem32 <= memoriaD(32) & memoriaD(33) & memoriaD(34) & memoriaD(35) &
+			memoriaD(36) & memoriaD(37) & memoriaD(38) & memoriaD(39);
+
+	mem40 <= memoriaD(40) & memoriaD(41) & memoriaD(42) & memoriaD(43) &
+			memoriaD(44) & memoriaD(45) & memoriaD(46) & memoriaD(47);
+
+	mem48 <= memoriaD(48) & memoriaD(49) & memoriaD(50) & memoriaD(51) &
+			memoriaD(52) & memoriaD(53) & memoriaD(54) & memoriaD(55);
+
+	mem56 <= memoriaD(56) & memoriaD(57) & memoriaD(58) & memoriaD(59) &
+			memoriaD(60) & memoriaD(61) & memoriaD(62) & memoriaD(63);
+
+	mem64 <= memoriaD(64) & memoriaD(65) & memoriaD(66) & memoriaD(67) &
+			memoriaD(68) & memoriaD(69) & memoriaD(70) & memoriaD(71);
+
+
 	process(clock)
 	begin 
 		if clock'event and clock='1' then 
